2b1c5ac11ada4a84f44faab5c215126f90571f36
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2024-02-02 11:55:20 +1100

    Adding from other repo

---
+++ Webflow-Sites-testing-variables/js/site-variables.js
@@ -0,0 +1,31 @@
+// Define your variables
+var companyName = "{{COMPANY_NAME}}";
+var phoneNumber = "{{PHONE_NUMBER}}";
+
+// Function to update company name
+function updateCompanyName() {
+    var elements = document.querySelectorAll('.company-name');
+    elements.forEach(function(element) {
+        element.textContent = companyName;
+    });
+}
+
+// Function to update phone number
+function updatePhoneNumber() {
+    var elements = document.querySelectorAll('.text-block-5, .text-block-5-copy, .phone_2, .phone_3, .phone_top');
+    elements.forEach(function(element) {
+        
+        if(element.tagName === 'DIV') {
+          element.textContent = phoneNumber; // Update text content
+        }
+        if(element.tagName === 'A') {
+          element.setAttribute('href', 'tel:' + phoneNumber);
+        }
+    });
+}
+
+// Call the functions to update content on page load
+document.addEventListener('DOMContentLoaded', function() {
+    updateCompanyName();
+    updatePhoneNumber();
+});
